bef6c0c4c7851e6538e90ee12203fb2a10360ada
Ahmed Morra <engahmedmorra@gmail.com>
2022-06-27 15:04:04 +0200

    - Person Movies Grid

--- lib/features/person_info/views/movies_grid.dart
+++ lib/features/person_info/views/movies_grid.dart
@@ -1,1 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class MoviesGrid extends StatelessWidget {
+  const MoviesGrid({
+    Key? key,
+    required this.moviesList,
+  }) : super(key: key);
+  final List<Map<String, String>> moviesList;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(
+            moviesList.length,
+            (index) => Padding(
+              padding: EdgeInsets.only(right: 5.w,),
+              child: SizedBox(
+                    width: 30.w,
+                    height: 60.w,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+
+                      children: [
+                        SizedBox(
+                          width: 30.w,
+                          height: 45.w,
+                          child: Image.network(moviesList[index]['poster']!),
+                        ),
+                        SizedBox(
+                          width: 30.w,
+                          height: 15.w,
+                          child: Text(
+                            moviesList[index]['name']!,
+                            style: const TextStyle(
+                              color: Colors.black,
+                            ),
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+            )),
+      ),
+    );
+  }
+}
--- lib/features/person_info/views/person_info_page.dart
+++ lib/features/person_info/views/person_info_page.dart
@@ -3,6 +3,8 @@ import 'package:popularpeople/features/person_info/models/person_info.dart';
 import 'package:popularpeople/features/person_info/views/profile_pictures_grid.dart';
 import 'package:sizer/sizer.dart';
 
+import 'movies_grid.dart';
+
 class PersonInfoPage extends StatelessWidget {
   const PersonInfoPage({
     Key? key,
@@ -23,7 +25,9 @@ class PersonInfoPage extends StatelessWidget {
               ProfilePicturesGrid(
                 profilePicturesLinks: profilePictureLinks,
               ),
-
+              MoviesGrid(
+                moviesList: moviesList,
+              ),
             ],
           ),
         ),
@@ -36,4 +40,23 @@ List<String?> profilePictureLinks = [
   'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2gHiYPXOq7RJLs4vTz5fUmSg8cd.jpg',
   'https://www.themoviedb.org/t/p/original/3nFpFd3Rt5pa9xkbF7MhwzLGbw8.jpg',
   'https://www.themoviedb.org/t/p/original/tmU9jtSs6c4ySC5eiad3aXXADan.jpg'
+];
+
+List<Map<String, String>> moviesList = [
+  {
+    'name': 'Once Upon a Time… in Hollywood',
+    'poster': 'https://www.themoviedb.org/t/p/w1280/8j58iEBw9pOXFD2L0nt0ZXeHviB.jpg',
+  },
+  {
+    'name': "The Dead Don't Die",
+    'poster': 'https://www.themoviedb.org/t/p/w1280/fgGzTEoNxptCRtEOpOPvIEdlxAq.jpg',
+  },
+  {
+    'name': 'Aliens in the Attic',
+    'poster': 'https://www.themoviedb.org/t/p/w1280/gjDfPcKcxdJlJkK6pgHrwDfSG3O.jpg',
+  },
+  {
+    'name': 'Dude',
+    'poster': 'https://www.themoviedb.org/t/p/w1280/vh4K737r0ZjrioIjHcs7ktV8RFO.jpg',
+  },
 ];
