f5970b72e616abfad1b26104c079342a354759ad
Ahmed Morra <engahmedmorra@gmail.com>
2022-06-29 19:52:52 +0200

    - Added Widget Test for personal info card

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,44 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:popularpeople/main.dart';
+import 'package:popularpeople/features/person_info/models/person_info.dart';
+import 'package:popularpeople/features/person_info/views/info_card.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('When person info card is rendered it shows person info', (WidgetTester tester) async {
+    PersonInfo personInfo = PersonInfo(
+      personId: '276543',
+      personName: 'Austin Butler',
+      personBirthday: '1991-08-17',
+      personBirthPlace: 'Anaheim, California, USA',
+      personKnownFor: 'Acting',
+      personBiography: "Austin Robert Butler (born August 17, 1991) is an American actor. He began his career on television, and had a guest role in two episodes of the teen show Hannah Montana in 2006 and 2007. He had a recurring role in the television series Switched at Birth (2011–2012) and main roles in The Carrie Diaries (2013–2014) and The Shannara Chronicles (2016–2017). In film, Butler played Tex Watson in Quentin Tarantino's Once Upon a Time in Hollywood (2019). In 2022, he gained critical acclaim for his portrayal of Elvis Presley in Baz Luhrman's musical biopic Elvis.",
+      personCredits: null,
+      personProfilePictures: null,
+    );
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      Sizer(builder: (
+          context,
+          orientation,
+          deviceType,
+          ) {
+        return MaterialApp(
+          home: Scaffold(
+            body: PersonInfoCard(
+              personInfo: personInfo,
+            ),
+          ),
+        );
+      })
+
+
+        );
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Austin Butler'), findsOneWidget);
+    expect(find.text('Acting'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
